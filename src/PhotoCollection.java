240825701e09da3cde4af06b5a49d033ab15ede7
Pascal Andermatt <p.andermatt@me.com>
2019-02-28 19:42:28 +0100

    Add code

--- src/PhotoCollection.java
+++ src/PhotoCollection.java
@@ -2,9 +2,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PhotoCollection {
+<<<<<<< Updated upstream
 
     List<Photo> photoCollection = new ArrayList<>();
     List<Slide> slideCollection = new ArrayList<>();
+=======
+    private List<Photo> photoCollection = new ArrayList<>();
+    private List<Slide> slideCollection = new ArrayList<>();
+>>>>>>> Stashed changes
 
     public void addPhoto(Photo photo) {
         photoCollection.add(photo);
@@ -14,11 +19,15 @@ public class PhotoCollection {
         return photoCollection;
     }
 
+<<<<<<< Updated upstream
     public List<Slide> getSlideCollection() {
         return slideCollection;
     }
 
     public void addSlide(Slide slide){
+=======
+    public void addSlide(Slide slide) {
+>>>>>>> Stashed changes
         slideCollection.add(slide);
     }
 }
