7663863c5f949eee46744fc1059eb7f866f6b2cf
Pascal Andermatt <p.andermatt@me.com>
2019-02-28 19:29:05 +0100

    Add stupid generator

--- src/PhotoCollection.java
+++ src/PhotoCollection.java
@@ -3,6 +3,7 @@ import java.util.List;
 
 public class PhotoCollection {
     List<Photo> photoCollection = new ArrayList<>();
+    List<Slide> slideCollection = new ArrayList<>();
 
     public void addPhoto(Photo photo) {
         photoCollection.add(photo);
@@ -10,5 +11,9 @@ public class PhotoCollection {
 
     public List<Photo> getPhotoCollection() {
         return photoCollection;
+    }
+
+    public void addSlide(Slide slide){
+        slideCollection.add(slide);
     }
 }
--- src/StupidSlideShowGenerator.java
+++ src/StupidSlideShowGenerator.java
@@ -1,9 +1,20 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class StupidSlideShowGenerator implements SlideShowGenerator {
 
     @Override
     public void generateSlideShow(PhotoCollection photoCollection) {
-        List<Photo> photos = photoCollection.getPhotoCollection();
+        List<Photo> photoList = photoCollection.getPhotoCollection();
+        List<Photo> slideOnPhoto = new ArrayList<>();
+
+        for (Photo photo : photoList) {
+            for (Photo comparePhoto : photoList) {
+                if (photo.compareTo(comparePhoto) > 1) {
+                    System.out.println("Photo found");
+                    photoCollection.addSlide(new Slide(slideOnPhoto));
+                }
+            }
+        }
     }
 }
